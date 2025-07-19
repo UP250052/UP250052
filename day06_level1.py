9f02df49c1883572b26428ac431c29b403ecfee4
UP250052 <up250052@alumnos.upa.edu.mx>
2025-07-18 23:12:10 -0600

    Add files via upload

---
+++ day06_level1.py
@@ -0,0 +1,44 @@
+#Exercises: Level 1
+print("Ejercicios nivel 1: ")
+#Ejercicio 1
+#Create an empty tuple
+print("Ejercicio 1:")
+empty_tutle = ()
+empty_tutle = tuple()
+
+
+
+
+#Ejercicio 2
+#Create a tuple containing names of your sisters and your brothers (imaginary siblings are fine)
+print("Ejercicio 2:")
+brothers= ("Junior, Felipe, Daniel,")
+sisters = ("Veronica")
+print("Los hermanos son:", brothers)
+print("Las hermanas son:", sisters)
+
+
+#Ejercicio 3
+#Join brothers and sisters tuples and assign it to siblings
+print("Ejercicio 3:")
+allbrothers = brothers +  sisters
+print ("Mis hermanos son:", allbrothers)
+
+
+#Ejercicio 4
+#How many siblings do you have?
+print("Ejercicio 4:")
+hermanos =("Junior, Felipe, Daniel, Veronica,")
+print("Yo tengo estos hermanos:")
+print(len(["Junior","Felipe","Daniel","Veronica",]))
+
+
+
+#Ejercicio 5
+#Modify the siblings tuple and add the name of your father and mother and assign it to family_members.
+print("Ejercicio 5:")
+hermanos = "Junior, Felipe, Daniel, Veronica,"
+padres =  "Mama Evelia, Papa Hector"
+family_members = hermanos +  padres 
+print( "Mi familia completa es: ", family_members)
+
